05123627ee90ee9b03c6e2ce2631500fb3a6894a
Vera Blok <xitraia@mail.ru>
2025-02-24 01:09:57 +0300

    junit4 added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,49 +1,49 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
-public class CashbackHackServiceTest {
+    public class CashbackHackServiceTest {
 
-    @Test
-    public void testRemainIfAmountIsZero() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 0;
-        int actual = cashbackHackService.remain(amount);
-        int expected = 1000;
-        assertEquals(actual,expected);
-    }
-    @Test
-    public void testRemainIfAmountIs1() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1;
-        int actual = cashbackHackService.remain(amount);
-        int expected = 999;
-        assertEquals(actual,expected);
-    }
-    @Test
-    public void testRemainIfAmountIs999() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 999;
-        int actual = cashbackHackService.remain(amount);
-        int expected = 1;
-        assertEquals(actual,expected);
-    }
-    @Test
-    public void testRemainIfAmountIs1000() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1000;
-        int actual = cashbackHackService.remain(amount);
-        int expected = 0;
-        assertEquals(actual,expected);
-    }
-    @Test
-    public void testRemainIfAmountIs1001() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1001;
-        int actual = cashbackHackService.remain(amount);
-        int expected = 999;
-        assertEquals(actual,expected);
-    }
+        @Test
+        public void testRemainIfAmountIsZero() {
+            CashbackHackService cashbackHackService = new CashbackHackService();
+            int amount = 0;
+            int actual = cashbackHackService.remain(amount);
+            int expected = 1000;
+            assertEquals(expected,actual);
+        }
+        @Test
+        public void testRemainIfAmountIs1() {
+            CashbackHackService cashbackHackService = new CashbackHackService();
+            int amount = 1;
+            int actual = cashbackHackService.remain(amount);
+            int expected = 999;
+            assertEquals(expected,actual);
+        }
+        @Test
+        public void testRemainIfAmountIs999() {
+            CashbackHackService cashbackHackService = new CashbackHackService();
+            int amount = 999;
+            int actual = cashbackHackService.remain(amount);
+            int expected = 1;
+            assertEquals(expected,actual);
+        }
+        @Test
+        public void testRemainIfAmountIs1000() {
+            CashbackHackService cashbackHackService = new CashbackHackService();
+            int amount = 1000;
+            int actual = cashbackHackService.remain(amount);
+            int expected = 0;
+            assertEquals(expected,actual);
+        }
+        @Test
+        public void testRemainIfAmountIs1001() {
+            CashbackHackService cashbackHackService = new CashbackHackService();
+            int amount = 1001;
+            int actual = cashbackHackService.remain(amount);
+            int expected = 999;
+            assertEquals(expected,actual);
+        }
 }
